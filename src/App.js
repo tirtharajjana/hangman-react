55ba6c749f8a0936e168f707c15e3e823e86414a
Tirtharaj <tirtharajjana615@gmail.com>
2021-07-25 22:18:41 +0530

    create all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+
 
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
+        <>
             <h1>Hangman</h1>
             <p>Find the hidden word - Enter a letter</p>
-        </div>
+        </>
     )
 }
 
