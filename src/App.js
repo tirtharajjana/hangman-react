60afd7ba336fc8d00dd4370943778bf2c6bccde9
Tirtharaj <tirtharajjana615@gmail.com>
2021-07-26 17:36:14 +0530

    display hangman body

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
@@ -20,13 +20,49 @@ const wrongLetters = [];
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([])
-  const [wrongLetters, setWrongLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handelKeydown = event => {
+      const { key, keyCode } = event;
+
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((correctLetters) => [...correctLetters, letter])
+
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter])
+
+          } else {
+            // showNotification();
+          }
+        }
+      }
+
+    }
+    window.addEventListener('keydown', handelKeydown);
+    return () => {
+      window.removeEventListener('keydown', handelKeydown)
+    }
+
+  }, [correctLetters, wrongLetters, playable])
+
+
+
+
   return (
     <>
       <Header />
       <div className="game-container">
-        <Figure />
-        <WrongLetters />
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word correctLetters={correctLetters} selectedWord={selectedWord} />
       </div>
     </>
